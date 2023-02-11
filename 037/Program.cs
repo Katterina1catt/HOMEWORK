ff9e8fcbfdb989457e3ae91772b7e38791f9479e
Katterina1catt <123167795+Katterina1catt@users.noreply.github.com>
2023-02-11 23:00:23 +0100

    Four

---
+++ 037/Program.cs
@@ -0,0 +1,19 @@
+﻿// 37. Компьютер придумывает число и определяет заканчивается ли на четную цифру на куб числа
+
+
+bool Test(int n)
+{
+    return n%2==0;
+//     if (n%2==0)
+//         return true;
+//     else
+//         return false;
+}
+Random random=new Random(10);
+for (int i=0;i<10;i++)
+{
+    int a=random.Next(1,100);
+    int b=(int)Math.Pow(a,3);
+    if (Test(a))
+    System.Console.WriteLine($"{a}, {b}");
+}
