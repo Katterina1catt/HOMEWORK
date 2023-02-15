00ed61e8a6c55e02ac008cd90da85e9218aeb103
Katterina1catt <123167795+Katterina1catt@users.noreply.github.com>
2023-02-15 12:31:57 +0100

    052

---
+++ BasicAlgorithms/5VS6/Program.cs
@@ -0,0 +1,19 @@
+﻿//NET.7 (C# 10, VB.NET, C++ CLI, IronPython).
+using System;
+namespace Example
+{
+ class Program
+{
+    static void Main()//точка входа
+    {
+        System.Console.WriteLine("Hello, World!");
+    }
+}
+}
+
+
+
+
+
+
+Console.WriteLine();
