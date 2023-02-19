7daf1d4e40ec2250c2d6e2aa9abe61d6b7c895c7
Katterina1catt <123167795+Katterina1catt@users.noreply.github.com>
2023-02-19 21:20:19 +0100

    lesson five

---
+++ 054/Program.cs
@@ -0,0 +1,20 @@
+﻿// 54. С клавиатуры вводится число N. 
+// Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+
+int N;
+N = Convert.ToInt32(Console.ReadLine());
+int[] f = new int[N];
+int a0=0;
+int a1=1;
+f[0]=a0;
+f[1]=a1;
+
+for(int i=2;i<N;i++)
+{
+    int a=a0+a1;
+    f[i]=a;
+    a0=a1;
+    a1=a;
+}
+for(int i=0;i<f.Length;i++)
+System.Console.Write($"{f[i]} ");
---
+++ 056/Program.cs
@@ -0,0 +1,17 @@
+﻿// 50. Написать программу копирования массива
+ 
+public class Example
+{
+    public static void Main()
+    {
+        int[] a={5, 4, 7, 2, 9 };
+ 
+        int[] copy=new int[a.Length];
+        for (int i=0;i < a.Length;i++) 
+        {
+            copy[i] = a[i];
+        }
+ 
+        Console.WriteLine(String.Join(", ", copy));
+    }
+}
