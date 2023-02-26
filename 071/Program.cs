47ef97efd8dfc98238d4e8f7a20f22adf515edfa
Katterina1catt <123167795+Katterina1catt@users.noreply.github.com>
2023-02-26 13:38:46 +0100

    Lesson 7

---
+++ 071/Program.cs
@@ -0,0 +1,9 @@
+﻿// 71. Показать натуральные числа от N до 1, N задано
+
+void Loop(int N, int i)
+{
+    System.Console.Write($"{N,4}");
+    if (N>i) Loop(N-1, i);
+}
+Loop(20,1);
+System.Console.WriteLine();
---
+++ 074/Program.cs
@@ -0,0 +1,8 @@
+﻿// 74. Найти сумму цифр числа
+
+int F(int N)
+{
+    if (N==0) return 0;
+    return N=N%10+F(N/10);
+}
+System.Console.WriteLine(F(465));
