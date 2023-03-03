10fa351077a24e581ee235cff32e51b03eb130cc
Katterina1catt <123167795+Katterina1catt@users.noreply.github.com>
2023-03-03 11:32:21 +0100

    lesson 9

---
+++ 098/Program.cs
@@ -0,0 +1,20 @@
+﻿// 98. Показать треугольник Паскаля
+//   *Сделать вывод в виде равнобедренного треугольника
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i,j,l=1;
+        for (i=1;i<=10;i++)
+        {
+            for (j=1;j<i+1;j++)
+            {
+                Console.Write(l++ +" ");
+            }
+ 
+            Console.Write("\n");
+        }
+        Console.ReadLine();
+    }
+}
